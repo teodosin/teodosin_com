5815b41ea0ceec1835fae84bd8d0b4de07beafa4
Viktor Teodosin <teodosin@protonmail.com>
2024-08-21 13:52:03 +0300

    dispatch

--- src/lib/utils/components/gallery-item.svelte
+++ src/lib/utils/components/gallery-item.svelte
@@ -10,10 +10,9 @@
    * @type {any}
    */
   export let selectedPost;
-  export let bcolor;
 </script>
 
-<a href={`${base}/${post.path}`}>
+<a class="post-wrapper" href={`${base}/${post.path}`}>
   <article
     class="post"
     on:mouseover={() => dispatch('select', post.path)}
@@ -34,12 +33,17 @@
 </a>
 
 <style>
+  .post-wrapper {
+    text-decoration: none;
+    color: inherit;
+    width: 100%;
+    height: 100%;
+  }
   .post {
     display: grid;
     aspect-ratio: 1/1;
     margin-bottom: 0rem;
     border: none;
-    overflow: hidden;
     align-content: end;
 
     position: relative;
@@ -51,7 +55,7 @@
     transition: opacity 0.3s ease;
   }
   .post:hover {
-    box-shadow: 0px 0px 88px 2px rgb(0, 0, 0);
+    box-shadow: 0px 0px 120px 2px rgb(0, 0, 0);
     transition: box-shadow 0.3s ease;
     transform: scale(
       1.005
@@ -62,6 +66,7 @@
     position: absolute;
     bottom: 0;
     background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0));
+    width: 100%;
     display: flex;
     flex-direction: column;
     align-content: end;
@@ -69,10 +74,19 @@
     padding-right: 1rem;
     color: #a7a7a7;
     transition: background 0.3s ease; /* Add transition for smooth effect */
+
+    transform: scaleY(0); /* Initially scale the description's height to 0 */
+    transform-origin: top; /* Set the origin of the transform to the top of the element */
+    transition:
+      max-height 0.2s ease,
+      transform 0.2s ease; /* Add transition for max-height and transform */
   }
   .post:hover .text-cont {
     background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.521));
     color: #f5f5f5;
+    max-height: 100px; /* On hover, increase the maximum height */
+    transform: scaleY(1.1); /* On hover, scale the description's height */
+    
   }
   .post.unselected {
     opacity: 0.2;
@@ -90,20 +104,8 @@
   }
   .post-desc {
     word-wrap: break-word;
-    max-height: 0; /* Initially set the maximum height to 0 */
     overflow: hidden; /* Hide the overflow */
     margin-top: 1.5rem;
-    transform: scaleY(0); /* Initially scale the description's height to 0 */
-    transform-origin: top; /* Set the origin of the transform to the top of the element */
-    transition:
-      max-height 0.2s ease,
-      transform 0.2s ease; /* Add transition for max-height and transform */
-  }
-  .post:hover .text-cont {
-    max-height: 100px; /* On hover, increase the maximum height */
-    transform: scaleY(
-      1
-    ); /* On hover, scale the description's height back to its original size */
   }
 
   @media (max-width: 600px) {
