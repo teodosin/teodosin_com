f8629b553b128651ed663ea9920f30ab81e9e1e0
Viktor Teodosin <teodosin@protonmail.com>
2024-08-25 20:16:00 +0300

    Updates

---
+++ _app/immutable/chunks/paths.C0GanQkm.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_7lkbud)==null?void 0:s.base)??"/teodosin";var e;const a=((e=globalThis.__sveltekit_7lkbud)==null?void 0:e.assets)??t;export{a,t as b};
---
+++ _app/immutable/entry/start.C0V6R9Lr.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.le56S9Kv.js";export{t as start};
